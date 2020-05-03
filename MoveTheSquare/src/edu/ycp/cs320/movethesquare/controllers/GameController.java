775c27acb3bc14e8828a3c568b8a656029422ab2
XOWWA <106664815+XOWWA@users.noreply.github.com>
2020-05-03 14:42:48 +0000

    Annotations.

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/controllers/GameController.java
@@ -6,3 +6,5 @@ import edu.ycp.cs320.movethesquare.model.Square;
 
 public class GameController {
 	public void computeSquareMoveDirection(Game game, Square square, double mouseX, double mouseY) {
+		if (mouseX >= 0 && mouseX < game.getWidth() && mouseY >= 0 && mouseY < game.getHeight()) {
+			double dx = mouseX - (square.getX() + square.getWidth()/2);
