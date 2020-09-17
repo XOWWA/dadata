fd115ed536202f37b432e960cf7fd9473fe2d904
XOWWA <106664815+XOWWA@users.noreply.github.com>
2020-09-17 05:24:49 +0000

    models update

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/model/Game.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/model/Game.java
@@ -1,4 +1,4 @@
 package edu.ycp.cs320.movethesquare.model;
 
 public class Game {
-	public static final double MOVE_DIST = 10.0; // x/y di
+	public static final double MOVE_DIST = 10.0; // x/y dista
