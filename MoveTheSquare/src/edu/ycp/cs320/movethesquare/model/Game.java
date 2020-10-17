4d2f9ebab91ecb76cecdea5f890311b4e5cf79e0
XOWWA <106664815+XOWWA@users.noreply.github.com>
2020-10-17 17:06:01 +0000

    Pontoon: Update Hungarian (hu) localization of Mozilla Advocacy

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/model/Game.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/model/Game.java
@@ -1,4 +1,5 @@
 package edu.ycp.cs320.movethesquare.model;
 
 public class Game {
-	public static final double MOVE_DIST = 10.0; // x/y distance square moves each tick
+	public static final double MOVE_DIST = 10.0; // x/y distance square moves each tick 
+	
