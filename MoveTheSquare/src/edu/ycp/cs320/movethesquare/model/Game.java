61b2b6fa221d027536054adddf9884efd9fd0e1f
XOWWA <106664815+XOWWA@users.noreply.github.com>
2020-11-06 05:43:53 +0000

    Update contact info

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/model/Game.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/model/Game.java
@@ -3,4 +3,4 @@ package edu.ycp.cs320.movethesquare.model;
 public class Game {
 	public static final double MOVE_DIST = 10.0; // x/y distance square moves each tick 
 	private double width, height;
-	private Squ
+	private Square
