3f764c429dd6ae67552355524e087a7309a605ac
XOWWA <106664815+XOWWA@users.noreply.github.com>
2022-12-30 07:29:57 +0000

    Auto generated

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -12,3 +12,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
+
+import edu.ycp.cs320.movethesquare.controllers.GameController;
+import edu.ycp.cs320.movethesquare.model.Game;
